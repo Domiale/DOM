47c5d7e88c883d7c7c94ddf4ec2d828135ec4e77
Domiale <163040102+Domiale@users.noreply.github.com>
2025-02-10 17:02:39 -0600

    Funcionalidad de agregar tarea

--- 11-Lista-de-tareas/script.js
+++ 11-Lista-de-tareas/script.js
@@ -1,7 +1,8 @@
 // Referemcias 
 
 const taskImput = document.getElementById("taskImput");
-const addButton = document.getElementById("addButton")
+const addButton = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
 
 // Escuchador
 
@@ -12,6 +13,32 @@ addButton.addEventListener("click" , createTask );
 function createTask () {
   if (taskImput.value){
     console.log(taskImput.value);
+
+    const taskItem = document.createElement("div");
+
+    taskItem.classList.add("task");
+
+    //Creamos un parrafo para la tarea 
+    const taskText = document.createElement("p");
+    taskText.innerText = taskImput.value;
+
+    //Creamos el contenedor de los iconos 
+    const taskIcons = document.createElement("div");
+    taskIcons.classList.add("task-icons");
+
+    //crear el icono check
+    const iconCheck = document.createElement("i");
+    iconCheck.classList.add("bi", "bi-check", "icon-complete");
+
+    //crear el icono trash
+    const iconDelate = document.createElement("i");
+    iconDelate.classList.add("bi", "bi-trash", "icon-delete");
+
+  //Estructura de los elementos
+  taskIcons.append(iconCheck, iconDelate);
+  taskItem.append(taskText, taskIcons);
+  taskList.append(taskItem);
+
     taskImput.value = ""; // regresamos el valor vacio  a Imput
   } else {
     alert("Hey, escribeme una tarea");
